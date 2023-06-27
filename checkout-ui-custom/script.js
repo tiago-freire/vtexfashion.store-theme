212a0c40ab81a02a1813feb5117a2e6d35670b64
Tiago de Andrade Freire <tiago.freire@cubos.io>
2023-06-27 16:21:00 -0300

    feat: improves nosto settings loading

--- checkout-ui-custom/script.js
+++ checkout-ui-custom/script.js
@@ -12,96 +12,120 @@ function waitElement(selector, fn) {
 }
 
 function updateNosto() {
-  $.getJSON('/_v/nosto-checkout/settings').done(nostoSettings => {
-    console.log('Nosto Settings:', nostoSettings)
+  let nostoSettings = JSON.parse(
+    localStorage.getItem('nosto-checkout-settings') ?? 'null'
+  )
 
-    const {
-      nostoAccountID,
-      enableNosto,
-      belowCartPlacementId,
-      belowSummaryPlacementId,
-    } = nostoSettings
-
-    if (nostoAccountID && enableNosto) {
-      if (!nostoEnabled) {
-        $('head').append(`
-                <script id="nostojs" type="text/javascript">
-                  (function(){var name="nostojs";window[name]=window[name]||function(cb){(window[name].q=window[name].q||[]).push(cb);};})();
-                </script>
-                <script src="https://connect.nosto.com/include/${nostoAccountID}" async></script>
-              `)
-        nostoEnabled = true
-      }
-
-      if (belowCartPlacementId && !$(`#${belowCartPlacementId}`).length) {
-        $('.cart-template-holder').append(
-          `<div class="nosto_element" id="${belowCartPlacementId}"></div>`
+  for (let i = 0; i < 3; i++) {
+    const getNostoSettingsTimeout = window.setTimeout(() => {
+      $.getJSON('/_v/nosto-checkout/settings').done(data => {
+        nostoSettings = data
+        localStorage.setItem(
+          'nosto-checkout-settings',
+          JSON.stringify(nostoSettings)
         )
-      }
-
-      if (belowSummaryPlacementId && !$(`#${belowSummaryPlacementId}`).length) {
-        $('.custom-cart-template-wrap').append(
-          `<div class="nosto_element" id="${belowSummaryPlacementId}"></div>`
-        )
-      }
-
-      nostojs(api => {
-        api
-          .defaultSession()
-          .setResponseMode('HTML')
-          .viewFrontPage()
-          .setPlacements(api?.placements?.getPlacements())
-          .load()
-          .then(response => {
-            console.log('Updating placements:', response)
-            api.placements.injectCampaigns(response.recommendations)
-            Object.keys(response.recommendations).forEach(key => {
-              $(`#${key} .nosto-header`).attr(
-                'style',
-                'font-family: inherit !important'
-              )
-            })
-          })
       })
+    }, 200)
 
-      waitElement('.nosto-header', () => {
-        $('.nosto-header').attr('style', 'font-family: inherit !important')
-      })
-
-      const clientProfileData = vtexjs?.checkout?.orderForm?.clientProfileData
-      const orderFormId = vtexjs?.checkout?.orderForm?.orderFormId
-      const nostoCustomer = {
-        customer_reference: orderFormId,
-        email: clientProfileData?.email,
-        first_name: clientProfileData?.firstName,
-        last_name: clientProfileData?.lastName,
-      }
-      const currencyCode =
-        vtexjs?.checkout?.orderForm?.storePreferencesData?.currencyCode
-      const nostoCartItems = vtexjs?.checkout?.orderForm?.items?.map(item => ({
-        name: item.name,
-        price_currency_code: currencyCode,
-        product_id: item.productId,
-        quantity: item.quantity,
-        sku_id: item.id,
-        unit_price: +item.sellingPrice / 100,
-      }))
-
-      nostojs?.(api => {
-        api
-          .defaultSession()
-          .setCart({ items: nostoCartItems })
-          .setCustomer(nostoCustomer)
-          .viewCart()
-          .update()
-          .then(data => {
-            console.log('Cart sent to Nosto:', nostoCartItems)
-            console.log('Customer sent to Nosto:', nostoCustomer)
-            console.log('Nosto updated data:', data)
-          })
-      })
+    if (nostoSettings) {
+      clearInterval(getNostoSettingsTimeout)
+      break
     }
-  })
+  }
+
+  if (nostoSettings) {
+    console.log('Nosto Settings:', nostoSettings)
+    processNostoSettings(nostoSettings)
+  }
+}
+
+function processNostoSettings(nostoSettings) {
+  const {
+    nostoAccountID,
+    enableNosto,
+    belowCartPlacementId,
+    belowSummaryPlacementId,
+  } = nostoSettings
+
+  if (nostoAccountID && enableNosto) {
+    if (!nostoEnabled) {
+      $('head').append(`
+              <script id="nostojs" type="text/javascript">
+                (function(){var name="nostojs";window[name]=window[name]||function(cb){(window[name].q=window[name].q||[]).push(cb);};})();
+              </script>
+              <script src="https://connect.nosto.com/include/${nostoAccountID}" async></script>
+            `)
+      nostoEnabled = true
+    }
+
+    if (belowCartPlacementId && !$(`#${belowCartPlacementId}`).length) {
+      $('.cart-template-holder').append(
+        `<div class="nosto_element" id="${belowCartPlacementId}"></div>`
+      )
+    }
+
+    if (belowSummaryPlacementId && !$(`#${belowSummaryPlacementId}`).length) {
+      $('.custom-cart-template-wrap').append(
+        `<div class="nosto_element" id="${belowSummaryPlacementId}"></div>`
+      )
+    }
+
+    nostojs(api => {
+      api
+        .defaultSession()
+        .setResponseMode('HTML')
+        .viewFrontPage()
+        .setPlacements(api?.placements?.getPlacements())
+        .load()
+        .then(response => {
+          console.log('Updating placements:', response)
+          api.placements.injectCampaigns(response.recommendations)
+          Object.keys(response.recommendations).forEach(key => {
+            $(`#${key} .nosto-header`).attr(
+              'style',
+              'font-family: inherit !important'
+            )
+          })
+        })
+    })
+
+    waitElement('.nosto-header', () => {
+      $('.nosto-header').attr('style', 'font-family: inherit !important')
+    })
+
+    const clientProfileData = vtexjs?.checkout?.orderForm?.clientProfileData
+    const orderFormId = vtexjs?.checkout?.orderForm?.orderFormId
+    const nostoCustomer = {
+      customer_reference: orderFormId,
+      email: clientProfileData?.email,
+      first_name: clientProfileData?.firstName,
+      last_name: clientProfileData?.lastName,
+    }
+    const currencyCode =
+      vtexjs?.checkout?.orderForm?.storePreferencesData?.currencyCode
+    const nostoCartItems = vtexjs?.checkout?.orderForm?.items?.map(item => ({
+      name: item.name,
+      price_currency_code: currencyCode,
+      product_id: item.productId,
+      quantity: item.quantity,
+      sku_id: item.id,
+      unit_price: +item.sellingPrice / 100,
+    }))
+
+    nostojs?.(api => {
+      api
+        .defaultSession()
+        .setCart({ items: nostoCartItems })
+        .setCustomer(nostoCustomer)
+        .viewCart()
+        .update()
+        .then(data => {
+          console.log('Cart sent to Nosto:', nostoCartItems)
+          console.log('Customer sent to Nosto:', nostoCustomer)
+          console.log('Nosto updated data:', data)
+        })
+    })
+  }
 }
 
 function updateDemoStoreWarning() {
